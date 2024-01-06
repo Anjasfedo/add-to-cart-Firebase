0d7055ade060417a8396588fac90906d71bca22e
AnjasFedo <fedoaf98@gmail.com>
2024-01-06 21:58:46 +0700

    Append Input to List

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import appSettings from "./config.js";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 
-const moviesInDB = ref(database, "movies")
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", () => {
   let inputValue = inputFieldEl.value;
 
-  push(moviesInDB, inputValue)
+  // push(shoppingListInDB, inputValue);
+
+  inputFieldEl.value = "";
+
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
 
   console.log(`${inputValue} Added to Database`);
 });
