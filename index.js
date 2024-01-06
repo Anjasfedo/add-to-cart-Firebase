9c83faa09fe20f97d1f7da6a01280446c8aa7091
AnjasFedo <fedoaf98@gmail.com>
2024-01-06 21:41:51 +0700

    Initialize Firebase Realtime Database

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import appSettings from "./config.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+
+const moviesInDB = ref(database, "movies")
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", () => {
   let inputValue = inputFieldEl.value;
-  console.log(inputValue);
+
+  push(moviesInDB, inputValue)
+
+  console.log(`${inputValue} Added to Database`);
 });
